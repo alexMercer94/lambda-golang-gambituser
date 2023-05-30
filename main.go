3e15f4a5a2a2d372cef4f808cd97120dd56a0fe7
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2023-05-30 15:15:14 -0600

    Configuracion AWS Lambda

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+
+	"backend/lambda-golang-gambituser/awsgo"
+
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(ExecuteLambda)
+}
+
+/*
+Ejecución de la Lambda
+*/
+func ExecuteLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
+	awsgo.InitializeAWS()
+}
